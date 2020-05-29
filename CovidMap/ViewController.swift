62eaf332a5b043e47cddb0dd1d6833dde2011a93
Michelle Lau <mdeniselau@gmail.com>
2020-05-29 20:43:43 +0900

    first commit

---
+++ CovidMap/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CovidMap
+//
+//  Created by Michelle Lau on 2020/05/29.
+//  Copyright © 2020 Michelle Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func makeAPICall() {
+//        let url = URL(string: "https://covid19-japan-web-api.now.sh/api/v1/prefectures")
+//        AF.request(url).validate().responseJSON { (response) in
+//            switch response.result {
+//            case .success(_):
+//                guard let value = response.value else { return }
+//                let json = JSON(value)
+//                for i in 0..<dataBranch.count {
+//                }
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//        }
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'CovidMap' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CovidMap
+  pod 'Alamofire', '~> 5.2'
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'FloatingPanel'
+
+
+
+
+end
